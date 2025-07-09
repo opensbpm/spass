0d72a27dd96df3d73277e6f69fb1cc3e1a59c1b1
Sedelmaier Stefan <stefan@sedelmaier.at>
2025-07-09 19:36:23 +0200

    Successful failing test

--- src/main/java/org/opensbpm/spass/SPassReader.java
+++ src/main/java/org/opensbpm/spass/SPassReader.java
@@ -2,8 +2,10 @@ package org.opensbpm.spass;
 
 import org.opensbpm.spass.model.ProcessModel;
 
+import java.io.InputStream;
+
 public class SPassReader {
-    public ProcessModel readFile(String s) {
+    public ProcessModel read(InputStream inputStream) {
         throw new UnsupportedOperationException("Not supported yet.");
     }
 }
--- src/test/java/org/opensbpm/spass/SpassReaderTest.java
+++ src/test/java/org/opensbpm/spass/SpassReaderTest.java
@@ -3,17 +3,21 @@ package org.opensbpm.spass;
 import org.junit.jupiter.api.Test;
 import org.opensbpm.spass.model.ProcessModel;
 
+import java.io.InputStream;
+
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.notNullValue;
 
 public class SpassReaderTest {
 
     @Test
-    public void testReadSpassFile() {
+    public void testReadSPassFile() {
         //arrange
+        InputStream inputStream = getClass().getResourceAsStream("/minimal.owl");
+        assert inputStream != null;
 
         //act
-        ProcessModel processModel = new SPassReader().readFile("src/test/resources/standard_PASS_ont_dev.owl");
+        ProcessModel processModel = new SPassReader().read(inputStream);
 
         //assert
         assertThat(processModel, notNullValue());
