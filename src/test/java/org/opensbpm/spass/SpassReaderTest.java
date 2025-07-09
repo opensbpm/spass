3b9a3667d2b511bb291a14efe51ca955aa3c3574
Sedelmaier Stefan <stefan@sedelmaier.at>
2025-07-09 19:23:01 +0200

    Initial version

---
+++ src/test/java/org/opensbpm/spass/SpassReaderTest.java
@@ -0,0 +1,11 @@
+package org.opensbpm.spass;
+
+import org.junit.jupiter.api.Test;
+
+public class SpassReaderTest {
+
+     @Test
+     public void testReadSpassFile() {
+         // Implement test logic here
+     }
+}
